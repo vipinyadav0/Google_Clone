c8c6117b24b858aab4af4349805ab79e5ef00db2
vipinyadav0 <vyvipinyadav998@gmail.com>
2021-12-22 16:01:12 +0530

    Django Sttings Modified

--- google_clone/google_clone/settings.py
+++ google_clone/google_clone/settings.py
@@ -17,7 +17,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-TEMPLATES_DIR = os.path.join(BASE_DIR)
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'google/templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/google/static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

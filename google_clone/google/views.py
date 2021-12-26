e7ee5ab548512c7ebc889fb736e44acc79d0e91a
vipinyadav0 <vyvipinyadav998@gmail.com>
2021-12-26 20:27:45 +0530

    Search Context added in views.py

--- google_clone/google/views.py
+++ google_clone/google/views.py
@@ -11,6 +11,7 @@ def search(request):
     if request.method=='POST':
 
         search = request.POST['search']
+        # contextt = {'search':search}
         url = 'https://www.ask.com/web?q=' + search
         result = requests.get(url)
         soup = bs(result.text, 'lxml')
@@ -18,16 +19,19 @@ def search(request):
         all_lists = soup.find_all('div',{'class':'PartialSearchResults-item'})
 
         final_result = []
+        search_k = []
+        search_k.append(search)
 
         for result in all_lists:
             query_title = result.find(class_='PartialSearchResults-item-title').text
             query_url = result.find('a').get('href')
             query_desc = result.find(class_='PartialSearchResults-item-abstract').text
 
-            final_result.append(query_title, query_url, query_desc)
+            final_result.append((query_title, query_url, query_desc))
 
         context ={
-            'final_result': final_result
+            'final_result': final_result,
+            'search':search_k
         }
         return render(request, 'search.html', context)
     else:
